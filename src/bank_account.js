accdf7a827c8bb7d0f406d847ce2eccf0c1ced8f
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-03-18 15:18:27 +0200

    Add some spacing

--- src/bank_account.js
+++ src/bank_account.js
@@ -7,10 +7,12 @@ class BankAccount {
     this.balance = new Decimal(0).toFixed(2);
     this.interestRate = new Decimal(interestRate);
   }
+
   deposit({ amount }) {
     assert(amount > 0, "Deposit amount must be positive");
     this.balance = new Decimal(this.balance).plus(amount).toFixed(2);
   }
+
   withdraw({ amount }) {
     assert(amount > 0, "Withdrawal amount must be positive");
     assert(
@@ -19,6 +21,7 @@ class BankAccount {
     );
     this.balance = new Decimal(this.balance).minus(amount).toFixed(2);
   }
+
   compoundInterest() {
     const monthlyInterest = new Decimal(this.balance)
       .times(this.interestRate)
